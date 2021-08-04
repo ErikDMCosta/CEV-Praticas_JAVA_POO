e6d1990f92f196de181816d9b9ff899472528056
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2021-08-04 17:12:55 -0300

    Add arquivos das aulas do curso Java POO via upload

---
+++ Aula12b/Aula12b.java
@@ -0,0 +1,36 @@
+package aula12b;
+
+public class Aula12b {
+
+          public static void main(String[] args) {
+                     // Animal n = new Animal();
+                     Mamifero m = new Mamifero();
+                     Reptil r = new Reptil();
+                     Peixe p = new Peixe();
+                     Ave a = new Ave();
+                     /*
+                     m.setPeso(35.3f);
+                     m.setCorPelo("Marrom");
+                     m.alimentar();
+                     m.locomover();
+                     m.emitirSom();
+                     
+                     a.locomover();
+                     p.locomover();
+                     r.locomover();
+                    */
+                     
+                     Canguru c = new Canguru();
+                     Cachorro k = new Cachorro();
+                     Cobra j = new Cobra();
+                     Tartaruga t = new Tartaruga();
+                     Goldfish g = new Goldfish();
+                     Arara e = new Arara();
+                     
+                     c.locomover();
+                     k.locomover();
+                     c.emitirSom();
+                     k.emitirSom();
+          }
+
+}
